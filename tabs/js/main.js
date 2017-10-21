a4dbd1396850b32f51fb18f7d37ab51a13c1c5fe
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-21 21:12:14 +0300

    Hello world app

---
+++ tabs/js/main.js
@@ -0,0 +1,19 @@
+﻿var throwEvent, on;
+(()=>{
+    var eventsStorage = {},
+        isFunction = function(obj) {
+            return !!(obj && obj.constructor && obj.call && obj.apply);
+        };
+    throwEvent = (event)=>{
+        if(eventsStorage[event]) eventsStorage[event].forEach((f)=>{
+            f();
+        });
+    };
+    on = (event, callback)=>{
+        if(isFunction(callback)){
+            if(eventsStorage[event]) eventsStorage[event].push(callback); else eventsStorage[event] = [callback];
+        } else {
+            console.error('Uncaught ' + (new TypeError('Seems like the callback is not an instance of function')).stack);
+        }
+    };
+})();
